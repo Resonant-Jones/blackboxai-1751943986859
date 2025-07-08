6cea22a8f0ef9fcd7b7c7f9d5339163aa0985b72
Resonant-Jones <jones@resonantconstructs.ai>
2025-07-07 23:07:15 -0400

    Add KinPod_v2/AppCore/KinPod.swift 1751944034685

---
+++ KinPod_v2/AppCore/KinPod.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct KinPod: App {
+    @StateObject private var appState = AppState()
+
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+                .environmentObject(appState)
+        }
+    }
+}
