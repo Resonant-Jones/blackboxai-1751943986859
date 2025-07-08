db9cdbc2ab0a8ca5e67d4a94dda11b3bd6a424ae
Resonant-Jones <jones@resonantconstructs.ai>
2025-07-07 23:07:15 -0400

    Add KinPod_v2/AppCore/ContentView.swift 1751944035310

---
+++ KinPod_v2/AppCore/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView0.swift
+//  KinPod_v2
+//
+//  Created by Christopher Castillo on 6/30/25.
+//
+
+//
+//  ContentView.swift
+//  KinPod_v2
+//
+//  Created by Christopher Castillo on 6/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        RootView()
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
